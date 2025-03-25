d8fe053b0ce3594f5b55f64572ab6b150f84370e
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-25 08:30:21 +0800

    Add queries for order items

--- common/Utility/SD.cs
+++ common/Utility/SD.cs
@@ -12,10 +12,12 @@ namespace inventory_system.common.Utility
         public const string SelectAllOrders = "SELECT o.order_id, o.created_at, CONCAT(c.first_name, ' ', c.last_name) AS customer_name, c.company_name, o.po_number, o.dr_number, o.total_price\r\nFROM orders o\r\nJOIN customers c ON o.customers_id = c.customers_id;";
         public const string InsertToOrders = "INSERT INTO orders (customers_id, po_number, dr_number, order_date, total_price, created_at) " +
                     "VALUES (@customer_id, @po_number, @dr_number, @order_date, @total_price, @created_at)";
+        public const string SelectOrder = "SELECT o.order_id, o.created_at, CONCAT(c.first_name, ' ', c.last_name) AS customer_name, c.company_name, o.po_number, o.dr_number, o.total_price\r\nFROM orders o\r\nJOIN customers c ON o.customers_id = c.customers_id WHERE o.order_id = @order_id";
         #endregion
         #region Order Items
-        public const string InsertToOrderItems = "INSERT INTO products (item_name, item_code, item_description, item_color, item_category, supplier, unit, stock, item_price, created_at) " +
-                               "VALUES (@item_name, @item_code, @item_description, @item_color, @item_category, @supplier, @unit, @stock, @item_price, @created_at)";
+        public const string InsertToOrderItems = "INSERT INTO order_items (order_id, product_id, quantity, price, created_at) " +
+                        "VALUES (@order_id, @product_id, @quantity, @price, @created_at)";
+        public const string SelectOrderItems = "SELECT product_id, quantity FROM order_items WHERE o.order_id = @order_id";
         #endregion
         #region Customers
         public const string SelectAllCustomers = "SELECT customers_id, CONCAT(first_name, ' ', last_name) AS customer_name, company_name, email FROM customers;";
