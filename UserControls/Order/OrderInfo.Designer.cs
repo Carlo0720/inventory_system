9c6b58e292367a6b63a29489312fb7e5c60eb389
Jesser Famorca <jesserfamorca@gmail.com>
2025-03-26 22:43:40 +0800

    Add order info layout for viewing order information

---
+++ UserControls/Order/OrderInfo.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace inventory_system.UserControls.Order
+{
+    partial class OrderInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            puchaseOrderLbl = new ReaLTaiizor.Controls.BigLabel();
+            deliveryReceiptLbl = new ReaLTaiizor.Controls.BigLabel();
+            orderItemsDatagridview = new ReaLTaiizor.Controls.PoisonDataGridView();
+            smallLabel1 = new ReaLTaiizor.Controls.SmallLabel();
+            totalQuantityLbl = new ReaLTaiizor.Controls.SmallLabel();
+            totalPriceLbl = new ReaLTaiizor.Controls.SmallLabel();
+            panel1 = new ReaLTaiizor.Controls.Panel();
+            button1 = new ReaLTaiizor.Controls.Button();
+            button2 = new ReaLTaiizor.Controls.Button();
+            button3 = new ReaLTaiizor.Controls.Button();
+            ((System.ComponentModel.ISupportInitialize)orderItemsDatagridview).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // puchaseOrderLbl
+            // 
+            puchaseOrderLbl.AutoSize = true;
+            puchaseOrderLbl.BackColor = Color.Transparent;
+            puchaseOrderLbl.Font = new Font("Segoe UI", 25F);
+            puchaseOrderLbl.ForeColor = Color.FromArgb(80, 80, 80);
+            puchaseOrderLbl.Location = new Point(3, 46);
+            puchaseOrderLbl.Name = "puchaseOrderLbl";
+            puchaseOrderLbl.Size = new Size(251, 46);
+            puchaseOrderLbl.TabIndex = 0;
+            puchaseOrderLbl.Text = "Purchase Order";
+            // 
+            // deliveryReceiptLbl
+            // 
+            deliveryReceiptLbl.AutoSize = true;
+            deliveryReceiptLbl.BackColor = Color.Transparent;
+            deliveryReceiptLbl.Font = new Font("Segoe UI", 25F);
+            deliveryReceiptLbl.ForeColor = Color.FromArgb(80, 80, 80);
+            deliveryReceiptLbl.Location = new Point(3, 0);
+            deliveryReceiptLbl.Name = "deliveryReceiptLbl";
+            deliveryReceiptLbl.Size = new Size(260, 46);
+            deliveryReceiptLbl.TabIndex = 1;
+            deliveryReceiptLbl.Text = "Delivery Receipt";
+            // 
+            // orderItemsDatagridview
+            // 
+            orderItemsDatagridview.AllowUserToResizeRows = false;
+            orderItemsDatagridview.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            orderItemsDatagridview.BackgroundColor = Color.FromArgb(255, 255, 255);
+            orderItemsDatagridview.BorderStyle = BorderStyle.None;
+            orderItemsDatagridview.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            orderItemsDatagridview.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(0, 174, 219);
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle4.ForeColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(0, 198, 247);
+            dataGridViewCellStyle4.SelectionForeColor = Color.FromArgb(17, 17, 17);
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            orderItemsDatagridview.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            orderItemsDatagridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle5.ForeColor = Color.FromArgb(136, 136, 136);
+            dataGridViewCellStyle5.SelectionBackColor = Color.FromArgb(0, 198, 247);
+            dataGridViewCellStyle5.SelectionForeColor = Color.FromArgb(17, 17, 17);
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.False;
+            orderItemsDatagridview.DefaultCellStyle = dataGridViewCellStyle5;
+            orderItemsDatagridview.EnableHeadersVisualStyles = false;
+            orderItemsDatagridview.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            orderItemsDatagridview.GridColor = Color.FromArgb(255, 255, 255);
+            orderItemsDatagridview.Location = new Point(3, 95);
+            orderItemsDatagridview.Name = "orderItemsDatagridview";
+            orderItemsDatagridview.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = Color.FromArgb(0, 174, 219);
+            dataGridViewCellStyle6.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle6.ForeColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(0, 198, 247);
+            dataGridViewCellStyle6.SelectionForeColor = Color.FromArgb(17, 17, 17);
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            orderItemsDatagridview.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            orderItemsDatagridview.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            orderItemsDatagridview.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            orderItemsDatagridview.Size = new Size(441, 279);
+            orderItemsDatagridview.TabIndex = 2;
+            // 
+            // smallLabel1
+            // 
+            smallLabel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            smallLabel1.AutoSize = true;
+            smallLabel1.BackColor = Color.Transparent;
+            smallLabel1.Font = new Font("Segoe UI", 8F);
+            smallLabel1.ForeColor = Color.FromArgb(142, 142, 142);
+            smallLabel1.Location = new Point(0, 139);
+            smallLabel1.Name = "smallLabel1";
+            smallLabel1.Size = new Size(32, 13);
+            smallLabel1.TabIndex = 3;
+            smallLabel1.Text = "Total";
+            // 
+            // totalQuantityLbl
+            // 
+            totalQuantityLbl.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            totalQuantityLbl.AutoSize = true;
+            totalQuantityLbl.BackColor = Color.Transparent;
+            totalQuantityLbl.Font = new Font("Segoe UI", 8F);
+            totalQuantityLbl.ForeColor = Color.FromArgb(142, 142, 142);
+            totalQuantityLbl.Location = new Point(59, 139);
+            totalQuantityLbl.Name = "totalQuantityLbl";
+            totalQuantityLbl.Size = new Size(79, 13);
+            totalQuantityLbl.TabIndex = 4;
+            totalQuantityLbl.Text = "Quantity Total";
+            // 
+            // totalPriceLbl
+            // 
+            totalPriceLbl.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            totalPriceLbl.AutoSize = true;
+            totalPriceLbl.BackColor = Color.Transparent;
+            totalPriceLbl.Font = new Font("Segoe UI", 8F);
+            totalPriceLbl.ForeColor = Color.FromArgb(142, 142, 142);
+            totalPriceLbl.Location = new Point(165, 139);
+            totalPriceLbl.Name = "totalPriceLbl";
+            totalPriceLbl.Size = new Size(59, 13);
+            totalPriceLbl.TabIndex = 5;
+            totalPriceLbl.Text = "Total Price";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(39, 51, 63);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(totalPriceLbl);
+            panel1.Controls.Add(smallLabel1);
+            panel1.Controls.Add(totalQuantityLbl);
+            panel1.EdgeColor = Color.FromArgb(32, 41, 50);
+            panel1.Location = new Point(3, 380);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(5);
+            panel1.Size = new Size(441, 172);
+            panel1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            panel1.TabIndex = 6;
+            panel1.Text = "panel1";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BorderColor = Color.FromArgb(32, 34, 37);
+            button1.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button1.EnteredColor = Color.FromArgb(32, 34, 37);
+            button1.Font = new Font("Microsoft Sans Serif", 12F);
+            button1.Image = null;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.InactiveColor = Color.FromArgb(32, 34, 37);
+            button1.Location = new Point(18, 21);
+            button1.Name = "button1";
+            button1.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button1.PressedColor = Color.FromArgb(165, 37, 37);
+            button1.Size = new Size(120, 40);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.TextAlignment = StringAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BorderColor = Color.FromArgb(32, 34, 37);
+            button2.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button2.EnteredColor = Color.FromArgb(32, 34, 37);
+            button2.Font = new Font("Microsoft Sans Serif", 12F);
+            button2.Image = null;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.InactiveColor = Color.FromArgb(32, 34, 37);
+            button2.Location = new Point(165, 21);
+            button2.Name = "button2";
+            button2.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button2.PressedColor = Color.FromArgb(165, 37, 37);
+            button2.Size = new Size(120, 40);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.TextAlignment = StringAlignment.Center;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BorderColor = Color.FromArgb(32, 34, 37);
+            button3.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button3.EnteredColor = Color.FromArgb(32, 34, 37);
+            button3.Font = new Font("Microsoft Sans Serif", 12F);
+            button3.Image = null;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.InactiveColor = Color.FromArgb(32, 34, 37);
+            button3.Location = new Point(313, 21);
+            button3.Name = "button3";
+            button3.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button3.PressedColor = Color.FromArgb(165, 37, 37);
+            button3.Size = new Size(120, 40);
+            button3.TabIndex = 8;
+            button3.Text = "button3";
+            button3.TextAlignment = StringAlignment.Center;
+            // 
+            // OrderInfo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Controls.Add(orderItemsDatagridview);
+            Controls.Add(deliveryReceiptLbl);
+            Controls.Add(puchaseOrderLbl);
+            Name = "OrderInfo";
+            Size = new Size(447, 555);
+            ((System.ComponentModel.ISupportInitialize)orderItemsDatagridview).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.BigLabel puchaseOrderLbl;
+        private ReaLTaiizor.Controls.BigLabel deliveryReceiptLbl;
+        private ReaLTaiizor.Controls.PoisonDataGridView orderItemsDatagridview;
+        private ReaLTaiizor.Controls.SmallLabel smallLabel1;
+        private ReaLTaiizor.Controls.SmallLabel totalQuantityLbl;
+        private ReaLTaiizor.Controls.SmallLabel totalPriceLbl;
+        private ReaLTaiizor.Controls.Panel panel1;
+        private ReaLTaiizor.Controls.Button button3;
+        private ReaLTaiizor.Controls.Button button2;
+        private ReaLTaiizor.Controls.Button button1;
+    }
+}
