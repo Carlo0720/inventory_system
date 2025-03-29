35012ee59296dd7541bdbce393114f18fca1f714
Jesser Famorca <jesserfamorca@gmail.com>
2025-03-29 09:14:39 +0800

    Add queries to `SD.cs` for updating orders, deleting items, and updating product stock

--- common/Utility/SD.cs
+++ common/Utility/SD.cs
@@ -17,11 +17,13 @@ namespace inventory_system.common.Utility
         public const string InsertToOrders = "INSERT INTO orders (customers_id, po_number, dr_number, order_date, total_price, created_at) " +
                     "VALUES (@customer_id, @po_number, @dr_number, @order_date, @total_price, @created_at)";
         public const string SelectOrder = "SELECT o.order_id, o.created_at, CONCAT(c.first_name, ' ', c.last_name) AS customer_name, c.company_name, o.po_number, o.dr_number, o.total_price\r\nFROM orders o\r\nJOIN customers c ON o.customers_id = c.customers_id WHERE o.order_id = @order_id";
+        public const string UpdateOrders = "UPDATE orders SET customer_id = @customer_id, po_number = @po_number, dr_number = @dr_number, total_price = @total_price, update_at = @updated_at WHERE order_id = @order_id";
         #endregion
         #region Order Items
         public const string InsertToOrderItems = "INSERT INTO order_items (order_id, product_id, quantity, price, created_at) " +
                         "VALUES (@order_id, @product_id, @quantity, @price, @created_at)";
         public const string SelectOrderItems = "SELECT product_id, quantity FROM order_items oi JOIN products p ON p.product_id = oi.product_id WHERE o.order_id = @order_id";
+        public const string DeleteOrderItems = "DELETE FROM order_items WHERE o.order_id = @order_id";
         #endregion
         #region Customers
         public const string SelectAllCustomersForOrder = "SELECT customers_id, CONCAT(first_name, ' ', last_name) AS customer_name, company_name, email FROM customers;";
@@ -29,6 +31,7 @@ namespace inventory_system.common.Utility
         #region Products
         public const string SelectProductsForOrder = "SELECT product_id, item_code, item_name, item_description, item_color, item_category, supplier, unit, stock, item_price FROM products";
         public const string SelectSpecificOrderProducts = "SELECT p.item_name, p.item_code, p.item_description, p.item_color, p.item_price, oi.quantity FROM order_items oi JOIN products p ON p.product_id = oi.product_id where oi.order_id = @order_id";
+        public const string UpdateProductStock = "UPDATE products SET stock = @stock WHERE product_id = @product_id";
         #endregion
     }
 }
